6c0aae43a97d8c28c14c633b84cb52823079a70a
guddnboy <hung3018@naver.com>
2025-09-09 16:56:47 +0900

    feat: 홈 화면 책장 및 버튼 추가

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-
-import Button from '../../components/Button';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import HomeHeader from './components/HomeHeader';
+import Bookshelf from './components/bookshelf/Bookshelf';
+import { TEMP_SCENARIOS } from '../../constants/tempData';
 
 export default function HomePage() {
   const navigate = useNavigate();
 
-  const handleNext = () => {
+  const handleStart = () => {
     navigate('/profile');
   };
 
   return (
-    <div className='flex flex-col items-center justify-center h-full'>
-      <h1>HomePage</h1>
-      <Button
-        className='bg-sjz-red-main w-80 h-12'
-        buttonText={'다음'}
-        onClick={handleNext}
-      />
+    <div className='flex flex-col h-full bg-white items-center'>
+      <HomeHeader />
+      <div className='flex flex-col items-center justify-between h-full my-8'>
+        <Bookshelf books={TEMP_SCENARIOS} />
+        <Button
+          className='w-[340px] h-[56px] bg-sjz-red-main text-white text-2xl font-bold rounded-xl'
+          buttonText='가족 등록'
+          onClick={handleStart}
+          icon='/icons/family-register.svg'
+        />
+      </div>
     </div>
   );
 }
