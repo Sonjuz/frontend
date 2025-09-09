f6a2bd134ee71f1bbd8e800775325568a123a525
guddnboy <hung3018@naver.com>
2025-09-09 15:51:55 +0900

    refactor: 레이아웃 컴포넌트 이름 수정

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-export default function Layout({ children }) {
+export default function DefaultLayout() {
   return (
     <div className='min-h-screen bg-gray-100 flex items-center justify-center p-2'>
       <div className='w-[393px] h-[753px] bg-white border-4 border-black rounded-4xl overflow-hidden shadow-2xl'>
-        {children}
+        <Outlet />
       </div>
     </div>
   );
