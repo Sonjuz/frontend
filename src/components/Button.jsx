f0bf294c7cbda0945261bf81b434b487496d3f4a
guddnboy <hung3018@naver.com>
2025-09-09 12:17:32 +0900

    feat: 버튼 컴포넌트 추가

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Button = ({ onClick, disabled, buttonText, className }) => {
+  return (
+    <button
+      className={`
+        cursor-pointer px-6 py-3
+        text-white rounded-xl
+        transition-all duration-200
+        focus:outline-none focus:ring-2 focus:ring-sjz-red-main focus:ring-offset-2
+        disabled:opacity-50 disabled:cursor-not-allowed
+        flex items-center justify-center
+        ${className}
+      `}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {buttonText}
+    </button>
+  );
+};
+
+export default Button;
