d58359ab4b45497c12abeab97faba6df2cc89c36
guddnboy <hung3018@naver.com>
2025-09-10 19:20:24 +0900

    feat: 프로필 등록 상단 헤더 추가

--- src/pages/register/ProfileRegisterPage.jsx
+++ src/pages/register/ProfileRegisterPage.jsx
@@ -4,6 +4,30 @@ import VoiceRegister from './components/step/VoiceRegister';
 import Done from './components/step/Done';
 import { ProgressBar } from './components/ProgressBar';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ProfileRegisterHeader = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className='flex w-full items-center p-4'>
+      <button onClick={handleBack} className='mr-2'>
+        <img
+          src='/icons/back-btn.svg'
+          alt='back'
+          className='w-10 h-10 cursor-pointer'
+        />
+      </button>
+      <div className='w-full flex'>
+        <div className='text-lg font-bold'>프로필 설정</div>
+      </div>
+    </div>
+  );
+};
 
 export default function RegisterPage() {
   const [currentStep, setCurrentStep] = useState(0);
@@ -21,6 +45,7 @@ export default function RegisterPage() {
 
   return (
     <div className='mx-auto max-w-md p-4 flex flex-col items-center justify-between h-full'>
+      <ProfileRegisterHeader />
       <ProgressBar steps={steps} currentStep={currentStep} />
       <div className='flex flex-col items-center justify-between h-full mt-6'>
         {currentStep === 0 && (
