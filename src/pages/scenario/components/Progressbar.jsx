0a7b8d5c550771785be94ccae7150a9ff41859a2
guddnboy <hung3018@naver.com>
2025-09-10 23:11:05 +0900

    feat: 시나리오 페이지에서 사용할 프로그레스 바 추가

---
+++ src/pages/scenario/components/Progressbar.jsx
@@ -0,0 +1,31 @@
+export const ProgressBar = ({ steps, currentStep = 0 }) => {
+  return (
+    <div className='relative h-12 w-80'>
+      <div className='absolute top-6 left-0 h-[1px] w-full bg-gray-200' />
+      <div className='relative flex h-full w-full items-center justify-between'>
+        {steps.map((step, index) => {
+          const isActive = index === currentStep;
+          const isCompleted = index < currentStep;
+
+          return (
+            <div key={index}>
+              {isActive ? (
+                <div className='bg-sjz-red-main/30 flex h-12 w-12 items-center justify-center rounded-full'>
+                  <div className='bg-sjz-red-main flex h-9 w-9 items-center justify-center rounded-full'>
+                    <span className='text-xl font-bold text-white'>{step}</span>
+                  </div>
+                </div>
+              ) : (
+                <div
+                  className={`h-6 w-6 rounded-full ${
+                    isCompleted ? 'bg-sjz-red-main' : 'bg-gray-200'
+                  }`}
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
