59463bcb764d1da65bc1c5c83542fcc236153ab6
guddnboy <hung3018@naver.com>
2025-09-10 22:17:28 +0900

    feat: url 복사 기능 추가

--- src/pages/register/components/step/Done.jsx
+++ src/pages/register/components/step/Done.jsx
@@ -1,4 +1,10 @@
-const ResultSection = () => {
+import { Button } from '../../../../components/Button';
+import { useNavigate } from 'react-router-dom';
+
+const ResultSection = ({ handleCopyUrl }) => {
+  const baseUrl = 'https://safeguard.app/profile/';
+  const tokenUrl = '2xa5f668pjf5mygmi';
+
   return (
     <div className='flex h-90 w-80 flex-col gap-y-2 rounded-2xl bg-white p-6 text-center shadow-xl outline-1 outline-offset-[-1px] outline-gray-100'>
       <div className='flex items-center justify-center'>
@@ -11,12 +17,18 @@ const ResultSection = () => {
         가족과 공유할 수 있는 전용 링크가 생성되었습니다
       </div>
       <div className='rounded-xl bg-gray-50 p-3'>
-        <div className='flex cursor-pointer flex-wrap justify-center text-lg text-blue-600 underline'>
-          <span>https://safeguard.app/profile/</span>
-          <span>2xa5f668pjf5mygmi</span>
+        <div
+          onClick={() => handleCopyUrl(baseUrl + tokenUrl)}
+          className='flex cursor-pointer flex-wrap justify-center text-lg text-blue-600 hover:underline'
+        >
+          <span>{baseUrl}</span>
+          <span>{tokenUrl}</span>
         </div>
       </div>
-      <button className='flex w-full items-center justify-center gap-2 rounded-xl bg-blue-100 px-4 py-3 text-lg font-medium text-blue-700'>
+      <button
+        onClick={() => handleCopyUrl(baseUrl + tokenUrl)}
+        className='flex w-full cursor-pointer items-center justify-center gap-2 rounded-xl bg-blue-100 px-4 py-3 text-lg font-medium text-blue-700'
+      >
         <img src='/icons/register-copy.svg' alt='복사' className='size-6' />
         URL 복사하기
       </button>
@@ -25,21 +37,39 @@ const ResultSection = () => {
 };
 
 export default function Done() {
+  const navigate = useNavigate();
+
+  const handleCopyUrl = url => {
+    navigator.clipboard.writeText(url);
+    console.log('URL 복사됨', url);
+  };
+
+  const navigateToHome = () => {
+    navigate('/');
+  };
+
   return (
-    <div className='flex flex-col items-center gap-8'>
-      <div className='w-80 text-center'>
-        <h1 className='mb-4 text-2xl font-bold text-gray-800'>
+    <div className='flex flex-col items-center justify-between gap-8'>
+      <div className='flex w-80 flex-col items-center gap-y-2 text-center'>
+        <div className='flex size-15 items-center justify-center rounded-full bg-gradient-to-br from-green-300 to-green-500'>
+          <img src='/icons/register-done.svg' alt='완료' className='size-10' />
+        </div>
+        <h1 className='text-2xl font-bold text-gray-800'>
           프로필 등록이 완료되었습니다!
         </h1>
-        <div className='whitespace-pre-line text-gray-600'>
+        <div className='text-lg break-keep whitespace-pre-line text-gray-600'>
           이제 가족의 목소리로 안전한 보이스피싱 방지 서비스를 이용할 수
           있습니다.
         </div>
       </div>
-      <div className='grid size-32 place-items-center rounded-full bg-gradient-to-br from-green-300 to-green-500'>
-        <img src='/icons/register-done.svg' alt='완료' className='size-12' />
-      </div>
-      <ResultSection />
+
+      <ResultSection handleCopyUrl={handleCopyUrl} />
+      <Button
+        onClick={navigateToHome}
+        className='bg-sjz-red-main h-15 w-full text-xl font-bold text-white'
+      >
+        홈으로 돌아가기
+      </Button>
     </div>
   );
 }
