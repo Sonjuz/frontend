87ebe01d2034c87f2e4b21b8e6857bf0115f769b
guddnboy <hung3018@naver.com>
2025-09-12 02:34:17 +0900

    fix: 애니메이션 효과 추가

--- src/pages/news/components/StoryCard.jsx
+++ src/pages/news/components/StoryCard.jsx
@@ -8,12 +8,8 @@ export default function StoryCard({
   image,
 }) {
   return (
-    <div className='flex h-120 w-full flex-col items-center gap-6 rounded-r-2xl border-2 bg-white p-4 pt-10 shadow-2xl'>
-      <img
-        src={image}
-        alt={title}
-        className='animate-swing-in-left-fwd size-40'
-      />
+    <div className='animate-swing-in-left-fwd flex h-120 w-full flex-col items-center gap-6 rounded-r-2xl border-2 bg-white p-4 pt-10 shadow-2xl'>
+      <img src={image} alt={title} className='size-40' />
       <h2 className='text-3xl font-bold text-gray-900 delay-100'>{title}</h2>
       <div className='flex items-center justify-center text-center text-2xl break-keep text-gray-700 delay-300'>
         {description}
